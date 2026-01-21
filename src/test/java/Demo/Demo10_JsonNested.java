c12413f68c35ed322d12454986bea3aaaaa3f7cf
Radhika50 <radhikardx12122004@gmail.com>
2026-01-21 16:57:44 +0530

    JSON Nested

---
+++ src/test/java/Demo/Demo10_JsonNested.java
@@ -0,0 +1,29 @@
+package Demo;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+public class Demo10_JsonNested {
+	@Test
+	public void f() throws JSONException {
+		  
+		   JSONObject parameters = new JSONObject();
+		   parameters.put("name", "John");
+		   parameters.put("Designation", "Test Engineer");
+		   parameters.put("id", 100);
+	 
+		   //Create new JSON Object to store contact details
+		   JSONObject details = new JSONObject();
+		   details.put("Official Number", 1234567890);
+		   details.put("Personal Number",1234567891);
+		   details.put("Land line", 223344);
+		   details.put("Email id", "abc@gmail.com");
+		 
+		   //make details as value to "Contact" Key    
+		   parameters.put("Contact", details);
+		 
+		   //print the JSON object as String in the console
+		   System.out.println(parameters.toString());
+		}
+}
